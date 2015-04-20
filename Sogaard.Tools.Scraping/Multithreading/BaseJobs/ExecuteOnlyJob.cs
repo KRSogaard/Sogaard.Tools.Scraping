caaba4f7a36a8d203c1022129ca2b307f03804be
Kasper Søgaard (Tradeshift) <krs@tradeshift.com>
2015-04-20 18:37:24 +0200

    Added a execute only base job

---
+++ Sogaard.Tools.Scraping/Multithreading/BaseJobs/ExecuteOnlyJob.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sogaard.Tools.Scraping.Multithreading.BaseJobs
+{
+    public abstract class ExecuteOnlyJob : IThreadedWebClientJob
+    {
+        public abstract List<IThreadedWebClientJob> Execute();
+        public abstract void FailedExecute(Exception exp);
+
+        public async Task ExecuteDownload(HttpClient client, CancellationToken cancelToken)
+        {
+            return;
+        }
+        public void FailedDownload(Exception exp)
+        {
+            return;
+        }
+
+    }
+}
