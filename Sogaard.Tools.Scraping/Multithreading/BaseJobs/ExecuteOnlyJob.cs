2f6674721b1c689d81c1430f198c8327c2d9156a
Kasper Rune Søgaard <kasper@sogaard.us>
2015-12-04 00:02:02 -0800

    Default ExecuteOnly FailedExecute

--- Sogaard.Tools.Scraping/Multithreading/BaseJobs/ExecuteOnlyJob.cs
+++ Sogaard.Tools.Scraping/Multithreading/BaseJobs/ExecuteOnlyJob.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using NLog;
 
 namespace Sogaard.Tools.Scraping.Multithreading.BaseJobs
 {
     public abstract class ExecuteOnlyJob : IThreadedWebClientJob
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public abstract List<IThreadedWebClientJob> Execute();
-        public abstract void FailedExecute(Exception exp);
+
+        public void FailedExecute(Exception exp)
+        {
+            logger.Error("Execute failed", exp);
+        }
 
         public async Task ExecuteDownload(HttpClient client, CancellationToken cancelToken)
         {
